813bf754597bf16ceb94a0ab89d2a30a15974951
黄智榕 <tonymgg@163.com>
2025-02-18 22:19:06 +0800

    Add basic support for grpc/grpc-go

--- multi_swe_bench/harness/repos/golang/__init__.py
+++ multi_swe_bench/harness/repos/golang/__init__.py
@@ -2,3 +2,4 @@ from multi_swe_bench.harness.repos.golang.cli import *
 from multi_swe_bench.harness.repos.golang.etcd_io import *
 from multi_swe_bench.harness.repos.golang.gin_gonic import *
 from multi_swe_bench.harness.repos.golang.go_gorm import *
+from multi_swe_bench.harness.repos.golang.grpc import *
