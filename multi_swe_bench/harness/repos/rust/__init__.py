220c10f88aff58786c91c555fc3129a36a7e6c5d
黄智榕 <tonymgg@163.com>
2025-02-19 00:52:06 +0800

    Add basic support for vectordotdev/vector

--- multi_swe_bench/harness/repos/rust/__init__.py
+++ multi_swe_bench/harness/repos/rust/__init__.py
@@ -1,3 +1,4 @@
 from multi_swe_bench.harness.repos.rust.clap_rs import *
 from multi_swe_bench.harness.repos.rust.rayon_rs import *
 from multi_swe_bench.harness.repos.rust.serde_rs import *
+from multi_swe_bench.harness.repos.rust.vectordotdev import *
