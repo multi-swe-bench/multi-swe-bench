0d706c5d502539157972eb29b3b220d5d3bb4ea2
黄智榕 <tonymgg@163.com>
2025-02-19 12:32:33 +0800

    Add basic support for tokio-rs/bytes

--- multi_swe_bench/harness/repos/rust/__init__.py
+++ multi_swe_bench/harness/repos/rust/__init__.py
@@ -1,4 +1,5 @@
 from multi_swe_bench.harness.repos.rust.clap_rs import *
 from multi_swe_bench.harness.repos.rust.rayon_rs import *
 from multi_swe_bench.harness.repos.rust.serde_rs import *
+from multi_swe_bench.harness.repos.rust.tokio_rs import *
 from multi_swe_bench.harness.repos.rust.vectordotdev import *
