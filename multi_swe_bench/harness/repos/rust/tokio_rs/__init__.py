739b396819fc08b4abe2975cb367a5d87968e087
黄智榕 <tonymgg@163.com>
2025-02-19 13:03:52 +0800

    Add basic support for tokio-rs/tracing

--- multi_swe_bench/harness/repos/rust/tokio_rs/__init__.py
+++ multi_swe_bench/harness/repos/rust/tokio_rs/__init__.py
@@ -1,3 +1,4 @@
 from multi_swe_bench.harness.repos.rust.tokio_rs.bytes import *
 from multi_swe_bench.harness.repos.rust.tokio_rs.mio import *
 from multi_swe_bench.harness.repos.rust.tokio_rs.tokio import *
+from multi_swe_bench.harness.repos.rust.tokio_rs.tracing import *
