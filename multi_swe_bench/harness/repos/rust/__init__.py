cefc33e9adc2afcaa1c9c190b71cd319db4c095d
黄智榕 <tonymgg@163.com>
2025-02-19 13:18:36 +0800

    Add basic support for typst/typst

--- multi_swe_bench/harness/repos/rust/__init__.py
+++ multi_swe_bench/harness/repos/rust/__init__.py
@@ -2,4 +2,5 @@ from multi_swe_bench.harness.repos.rust.clap_rs import *
 from multi_swe_bench.harness.repos.rust.rayon_rs import *
 from multi_swe_bench.harness.repos.rust.serde_rs import *
 from multi_swe_bench.harness.repos.rust.tokio_rs import *
+from multi_swe_bench.harness.repos.rust.typst import *
 from multi_swe_bench.harness.repos.rust.vectordotdev import *
