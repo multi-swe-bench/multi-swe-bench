9c0c06fe48052d1c68abd0053545c7379edcc103
黄智榕 <tonymgg@163.com>
2025-02-19 20:58:25 +0800

    Add basic support for alibaba/fastjson2

--- multi_swe_bench/harness/repos/__init__.py
+++ multi_swe_bench/harness/repos/__init__.py
@@ -1,4 +1,5 @@
 from multi_swe_bench.harness.repos.cpp import *
 from multi_swe_bench.harness.repos.golang import *
+from multi_swe_bench.harness.repos.java import *
 from multi_swe_bench.harness.repos.rust import *
 from multi_swe_bench.harness.repos.typescript import *
