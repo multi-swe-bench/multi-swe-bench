4aafe3feda2647993e0ab1fa76b2282ef873de45
HagonChan <2422907189@qq.com>
2025-02-22 22:18:31 +0800

    fix __init__

--- multi_swe_bench/harness/repos/c/__init__.py
+++ multi_swe_bench/harness/repos/c/__init__.py
@@ -2,5 +2,4 @@ from multi_swe_bench.harness.repos.c.valkey_io import *
 from multi_swe_bench.harness.repos.c.netdata import *
 from multi_swe_bench.harness.repos.c.facebook import *
 from multi_swe_bench.harness.repos.c.ponylang import *
-from multi_swe_bench.harness.repos.c.hashcat import *
 from multi_swe_bench.harness.repos.c.radareorg import *
