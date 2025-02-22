d419a01bbe4746385c8af523ebf2ac506ac2701f
黄智榕 <tonymgg@163.com>
2025-02-22 20:24:21 +0800

    Add basic support for apache/pulsar

--- multi_swe_bench/harness/repos/java/__init__.py
+++ multi_swe_bench/harness/repos/java/__init__.py
@@ -1,3 +1,4 @@
 from multi_swe_bench.harness.repos.java.alibaba import *
+from multi_swe_bench.harness.repos.java.apache import *
 from multi_swe_bench.harness.repos.java.elastic import *
 from multi_swe_bench.harness.repos.java.junit_team import *
