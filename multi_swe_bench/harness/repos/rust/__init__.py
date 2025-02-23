3bea91ecf2adac16abaeb549163d7983ffc3e631
黄智榕 <tonymgg@163.com>
2025-02-23 16:23:59 +0800

    Add basic support for nushell/nushell

--- multi_swe_bench/harness/repos/rust/__init__.py
+++ multi_swe_bench/harness/repos/rust/__init__.py
@@ -2,6 +2,7 @@ from multi_swe_bench.harness.repos.rust.astral_sh import *
 from multi_swe_bench.harness.repos.rust.BurntSushi import *
 from multi_swe_bench.harness.repos.rust.clap_rs import *
 from multi_swe_bench.harness.repos.rust.denoland import *
+from multi_swe_bench.harness.repos.rust.nushell import *
 from multi_swe_bench.harness.repos.rust.rayon_rs import *
 from multi_swe_bench.harness.repos.rust.serde_rs import *
 from multi_swe_bench.harness.repos.rust.sharkdp import *
