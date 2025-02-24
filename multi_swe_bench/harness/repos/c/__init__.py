120e0f50b7d85bf4efa8d39b94cf1e7ccf8b9369
HagonChan <2422907189@qq.com>
2025-02-25 00:48:31 +0800

    updata c cpp js

--- multi_swe_bench/harness/repos/c/__init__.py
+++ multi_swe_bench/harness/repos/c/__init__.py
@@ -1,3 +1,5 @@
 from multi_swe_bench.harness.repos.c.facebook import *
 from multi_swe_bench.harness.repos.c.ponylang import *
 from multi_swe_bench.harness.repos.c.valkey_io import *
+from multi_swe_bench.harness.repos.c.jqlang import *
+from multi_swe_bench.harness.repos.c.redis import *
---
+++ multi_swe_bench/harness/repos/javascript/__init__.py
@@ -0,0 +1,7 @@
+from multi_swe_bench.harness.repos.javascript.anuraghazra import *
+from multi_swe_bench.harness.repos.javascript.axios import *
+from multi_swe_bench.harness.repos.javascript.expressjs import *
+from multi_swe_bench.harness.repos.javascript.iamkun import *
+from multi_swe_bench.harness.repos.javascript.Kong import *
+from multi_swe_bench.harness.repos.javascript.preactjs import *
+from multi_swe_bench.harness.repos.javascript.sveltejs import *
