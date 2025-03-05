d8bf5f3c35ef348b414c5a621f05aaf699b36743
25626 <2562656746@qq.com>
2025-03-05 17:30:31 +0800

    update

--- multi_swe_bench/harness/repos/cpp/bitcoin/bitcoin.py
+++ multi_swe_bench/harness/repos/cpp/bitcoin/bitcoin.py
@@ -119,6 +119,58 @@ RUN apt-get install -y qtbase5-dev qttools5-dev qttools5-dev-tools qtwayland5 li
 
 """
 
+class bitcoinImageBaseCpp11V2(Image):
+    def __init__(self, pr: PullRequest, config: Config):
+        self._pr = pr
+        self._config = config
+
+    @property
+    def pr(self) -> PullRequest:
+        return self._pr
+
+    @property
+    def config(self) -> Config:
+        return self._config
+
+    def dependency(self) -> Union[str, "Image"]:
+        return "gcc:11"
+
+    def image_name(self) -> str:
+        return f"{self.pr.org}/{self.pr.repo}".lower()
+
+    def image_tag(self) -> str:
+        return "base-cpp-11V2"
+
+    def workdir(self) -> str:
+        return "base-cpp-11V2"
+
+    def files(self) -> list[File]:
+        return []
+
+    def dockerfile(self) -> str:
+        image_name = self.dependency()
+        if isinstance(image_name, Image):
+            image_name = image_name.image_full_name()
+
+        if self.config.need_clone:
+            code = f"RUN git clone https://github.com/{self.pr.org}/{self.pr.repo}.git /home/{self.pr.repo}"
+        else:
+            code = f"COPY {self.pr.repo} /home/{self.pr.repo}"
+
+        return f"""FROM {image_name}
+
+{self.global_env}
+
+WORKDIR /home/
+ENV DEBIAN_FRONTEND=noninteractive
+ENV TZ=Etc/UTC
+{code}
+RUN apt-get update && apt-get install -y libtool autotools-dev automake pkg-config bsdmainutils python3 libevent-dev libboost-dev libboost-system-dev libboost-filesystem-dev libboost-test-dev libsqlite3-dev libminiupnpc-dev libnatpmp-dev libzmq3-dev systemtap-sdt-dev python3-zmq
+RUN apt-get install -y qtbase5-dev qttools5-dev qttools5-dev-tools qtwayland5 libqrencode-dev
+RUN apt-get install -y libdb-dev libdb++-dev
+{self.clear_env}
+
+"""
 
 class bitcoinImageDefault(Image):
     def __init__(self, pr: PullRequest, config: Config):
@@ -134,9 +186,10 @@ class bitcoinImageDefault(Image):
         return self._config
 
     def dependency(self) -> Image | None:
-        if self.pr.number <= 30762:
+        if 24138<= self.pr.number <= 30762:
             return bitcoinImageBaseCpp11(self.pr, self._config)
-
+        elif self.pr.number <= 24104:
+            return bitcoinImageBaseCpp11V2(self.pr, self._config)
         return bitcoinImageBase(self.pr, self._config)
 
     def image_name(self) -> str:
@@ -149,7 +202,7 @@ class bitcoinImageDefault(Image):
         return f"pr-{self.pr.number}"
 
     def files(self) -> list[File]:
-        if self.pr.number <= 30762:
+        if 24138<=self.pr.number <= 30762:
             return [
                 File(
                     ".",
@@ -242,6 +295,107 @@ cd /home/{pr.repo}
 git apply --whitespace=nowarn /home/test.patch /home/fix.patch
 ./autogen.sh
 ./configure
+make check -j 4
+test/functional/test_runner.py --extended
+
+    """.format(
+                        pr=self.pr
+                    ),
+                ),
+            ]
+        elif self.pr.number <= 24104:
+            return [
+                File(
+                    ".",
+                    "fix.patch",
+                    f"{self.pr.fix_patch}",
+                ),
+                File(
+                    ".",
+                    "test.patch",
+                    f"{self.pr.test_patch}",
+                ),
+                File(
+                    ".",
+                    "check_git_changes.sh",
+                    """#!/bin/bash
+set -e
+
+if ! git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
+  echo "check_git_changes: Not inside a git repository"
+  exit 1
+fi
+
+if [[ -n $(git status --porcelain) ]]; then
+  echo "check_git_changes: Uncommitted changes"
+  exit 1
+fi
+
+echo "check_git_changes: No uncommitted changes"
+exit 0
+
+    """.format(
+                        pr=self.pr
+                    ),
+                ),
+                File(
+                    ".",
+                    "prepare.sh",
+                    """#!/bin/bash
+set -e
+
+cd /home/{pr.repo}
+git reset --hard
+bash /home/check_git_changes.sh
+git checkout {pr.base.sha}
+bash /home/check_git_changes.sh
+
+    """.format(
+                        pr=self.pr
+                    ),
+                ),
+                File(
+                    ".",
+                    "run.sh",
+                    """#!/bin/bash
+set -e
+
+cd /home/{pr.repo}
+./autogen.sh
+./configure --with-incompatible-bdb
+make check -j 4
+test/functional/test_runner.py --extended
+    """.format(
+                        pr=self.pr
+                    ),
+                ),
+                File(
+                    ".",
+                    "test-run.sh",
+                    """#!/bin/bash
+set -e
+
+cd /home/{pr.repo}
+git apply --whitespace=nowarn /home/test.patch
+./autogen.sh
+./configure --with-incompatible-bdb
+make check -j 4
+test/functional/test_runner.py --extended
+
+    """.format(
+                        pr=self.pr
+                    ),
+                ),
+                File(
+                    ".",
+                    "fix-run.sh",
+                    """#!/bin/bash
+set -e
+
+cd /home/{pr.repo}
+git apply --whitespace=nowarn /home/test.patch /home/fix.patch
+./autogen.sh
+./configure --with-incompatible-bdb
 make check -j 4
 test/functional/test_runner.py --extended
 
