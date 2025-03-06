819e07718544bca444885b524f9639432c44d2ae
HagonChan <2422907189@qq.com>
2025-03-06 18:59:27 +0800

    add ts/chakra_ui

--- multi_swe_bench/harness/repos/__init__.py
+++ multi_swe_bench/harness/repos/__init__.py
@@ -5,3 +5,5 @@ from multi_swe_bench.harness.repos.java import *
 from multi_swe_bench.harness.repos.javascript import *
 from multi_swe_bench.harness.repos.rust import *
 from multi_swe_bench.harness.repos.typescript import *
+from multi_swe_bench.harness.repos.typescript.chakra_ui import *
+
