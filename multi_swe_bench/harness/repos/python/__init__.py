59601d972eb22a6500d50e49cfabad7d08a85b08
aoyanli <aoyanli.722@bytedance.com>
2025-08-04 00:50:27 +0000

    Update files by envagent

--- multi_swe_bench/harness/repos/python/__init__.py
+++ multi_swe_bench/harness/repos/python/__init__.py
@@ -57,3 +57,5 @@ from multi_swe_bench.harness.repos.python.qiskit import *
 from multi_swe_bench.harness.repos.python.networkx import *
 from multi_swe_bench.harness.repos.python.plotly import *
 from multi_swe_bench.harness.repos.python.vega import *
+from multi_swe_bench.harness.repos.python.qiboteam import *
+from multi_swe_bench.harness.repos.python.radiasoft import *
