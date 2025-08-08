c1911d16f0979b5af5b5fc48390ad8592c204bff
aoyanli <aoyanli.722@bytedance.com>
2025-08-08 05:47:04 +0000

    Update files by envagent

--- multi_swe_bench/harness/repos/python/__init__.py
+++ multi_swe_bench/harness/repos/python/__init__.py
@@ -58,5 +58,10 @@ from multi_swe_bench.harness.repos.python.networkx import *
 from multi_swe_bench.harness.repos.python.plotly import *
 from multi_swe_bench.harness.repos.python.vega import *
 from multi_swe_bench.harness.repos.python.sqlfluff import *
-from multi_swe_bench.harness.repos.python.skypilot_org import *
-from multi_swe_bench.harness.repos.python.skypilot_org import *
+from multi_swe_bench.harness.repos.python.sqlfluff import *
+from multi_swe_bench.harness.repos.python.sqlfluff import *
+from multi_swe_bench.harness.repos.python.sqlfluff import *
+from multi_swe_bench.harness.repos.python.sqlfluff import *
+from multi_swe_bench.harness.repos.python.sqlfluff import *
+from multi_swe_bench.harness.repos.python.sqlfluff import *
+from multi_swe_bench.harness.repos.python.sqlfluff import *
