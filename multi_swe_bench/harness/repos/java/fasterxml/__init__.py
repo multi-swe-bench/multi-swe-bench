dca43e8e3727935a058979dcb24734905b81ecd8
aoyanli <aoyanli.722@bytedance.com>
2025-08-19 22:27:47 +0000

    Update files by envagent

--- multi_swe_bench/harness/repos/java/fasterxml/__init__.py
+++ multi_swe_bench/harness/repos/java/fasterxml/__init__.py
@@ -1,3 +1,5 @@
 from multi_swe_bench.harness.repos.java.fasterxml.jackson_core import *
 from multi_swe_bench.harness.repos.java.fasterxml.jackson_databind import *
 from multi_swe_bench.harness.repos.java.fasterxml.jackson_dataformat_xml import *
+from multi_swe_bench.harness.repos.java.fasterxml.jackson_databind_5022_to_unknown import *
+from multi_swe_bench.harness.repos.java.fasterxml.jackson_dataformat_xml_126_to_126 import *
