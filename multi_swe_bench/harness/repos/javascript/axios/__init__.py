2d8cd42c6893f874ed58619f65a7bde002c5bf45
aoyanli <aoyanli.722@bytedance.com>
2025-08-20 03:27:02 +0000

    Update files by envagent

--- multi_swe_bench/harness/repos/javascript/axios/__init__.py
+++ multi_swe_bench/harness/repos/javascript/axios/__init__.py
@@ -1,1 +1,4 @@
 from multi_swe_bench.harness.repos.javascript.axios.axios import *
+from multi_swe_bench.harness.repos.javascript.axios.axios_6091_to_unknown import *
+from multi_swe_bench.harness.repos.javascript.axios.axios_3852_to_1655 import *
+from multi_swe_bench.harness.repos.javascript.axios.axios_275_to_160 import *
