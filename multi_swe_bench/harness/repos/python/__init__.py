28ff513531140d64d93403a4613135436ba4835a
aoyanli <aoyanli.722@bytedance.com>
2025-08-29 11:35:17 +0000

    Update files by envagent

--- multi_swe_bench/harness/repos/python/__init__.py
+++ multi_swe_bench/harness/repos/python/__init__.py
@@ -127,4 +127,3 @@ from multi_swe_bench.harness.repos.python.RasaHQ import *
 from multi_swe_bench.harness.repos.python.pyccel import *
 from multi_swe_bench.harness.repos.python.buildbot import *
 from multi_swe_bench.harness.repos.python.cal_itp import *
-from multi_swe_bench.harness.repos.python.bookwyrm_social import *
