52f88bad27a32e1fe7f4e77b43631ca5fea577c9
aoyanli <aoyanli.722@bytedance.com>
2025-09-03 12:57:35 +0000

    Update files by envagent

--- multi_swe_bench/harness/repos/python/__init__.py
+++ multi_swe_bench/harness/repos/python/__init__.py
@@ -125,3 +125,6 @@ from multi_swe_bench.harness.repos.python.Clinical_Genomics import *
 from multi_swe_bench.harness.repos.python.pymc_devs import *
 from multi_swe_bench.harness.repos.python.RasaHQ import *
 from multi_swe_bench.harness.repos.python.pyccel import *
+from multi_swe_bench.harness.repos.python.HyphaApp import *
+from multi_swe_bench.harness.repos.python.LiberTEM import *
+from multi_swe_bench.harness.repos.python.hyperspy import *
