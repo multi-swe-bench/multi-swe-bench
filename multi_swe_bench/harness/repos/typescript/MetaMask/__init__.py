7c6f2ecab82f202b938b46d39e74c6083ccb65cc
aoyanli <aoyanli.722@bytedance.com>
2025-09-17 06:30:32 +0000

    Update files by envagent

--- multi_swe_bench/harness/repos/typescript/MetaMask/__init__.py
+++ multi_swe_bench/harness/repos/typescript/MetaMask/__init__.py
@@ -7,3 +7,7 @@ from multi_swe_bench.harness.repos.typescript.MetaMask.snaps_905_to_unknown impo
 from multi_swe_bench.harness.repos.typescript.MetaMask.snaps_878_to_unknown import *
 from multi_swe_bench.harness.repos.typescript.MetaMask.core_3660_to_2050 import *
 from multi_swe_bench.harness.repos.typescript.MetaMask.metamask_mobile_3811_to_3458 import *
+from multi_swe_bench.harness.repos.typescript.MetaMask.metamask_mobile_2703_to_unknown import *
+from multi_swe_bench.harness.repos.typescript.MetaMask.metamask_mobile_2109_to_1793 import *
+from multi_swe_bench.harness.repos.typescript.MetaMask.metamask_mobile_1481_to_1060 import *
+from multi_swe_bench.harness.repos.typescript.MetaMask.metamask_mobile_759_to_753 import *
