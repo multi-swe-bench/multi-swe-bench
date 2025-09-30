a831a1ed53016570b254f86b363daa7fc65dd552
aoyanli <aoyanli.722@bytedance.com>
2025-09-30 15:41:18 +0000

    Update files by envagent

--- multi_swe_bench/harness/repos/typescript/__init__.py
+++ multi_swe_bench/harness/repos/typescript/__init__.py
@@ -26,3 +26,4 @@ from multi_swe_bench.harness.repos.typescript.vuetifyjs import *
 from multi_swe_bench.harness.repos.typescript.highcharts import *
 from multi_swe_bench.harness.repos.typescript.League_of_Foundry_Developers import *
 from multi_swe_bench.harness.repos.typescript.VKCOM import *
+from multi_swe_bench.harness.repos.typescript.defenseunicorns import *
