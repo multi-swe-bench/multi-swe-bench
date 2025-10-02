42490208aa5f486c0e9473c3b6afe15a6c2ab51f
aoyanli <aoyanli.722@bytedance.com>
2025-10-02 11:29:04 +0000

    Update files by envagent

--- multi_swe_bench/harness/repos/javascript/__init__.py
+++ multi_swe_bench/harness/repos/javascript/__init__.py
@@ -27,3 +27,4 @@ from multi_swe_bench.harness.repos.javascript.alibabafusion import *
 from multi_swe_bench.harness.repos.javascript.carbondesignsystem import *
 from multi_swe_bench.harness.repos.javascript.quartodev import *
 from multi_swe_bench.harness.repos.javascript.FlowFuse import *
+from multi_swe_bench.harness.repos.javascript.brave import *
